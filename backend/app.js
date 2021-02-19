e458ee9632bc7e7aab5947633e8891071176b9bb
wissal <wissal.senussi@gmail.com>
2021-02-19 16:03:25 +0100

    app

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://@cluster0.sziyq.mongodb.net/projet6?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to Mongo DB Atlas');
+        console.error(error);
+    });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+
+
+module.exports = app;
